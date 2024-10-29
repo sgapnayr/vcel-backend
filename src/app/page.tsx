ff677c73948a4e7037be6ab498ba28bb3d660f31
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-10-29 15:11:34 -0500

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,7 @@
 "use client";
 
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { User } from "../../types/type";
-
 export default function Home() {
-  const [, setData] = useState<User>();
-
-  async function getData() {
-    try {
-      const res = await axios.post("/api/user");
-      setData(res.data as User);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  async function postData() {
-    const res = await axios.post("/api/email", {
-      email: "tefdsafdsafssast@gmail.com",
-      name: "Ryan",
-    });
-    console.log(res.data);
-  }
-
   return (
-    <div className="flex min-h-screen w-full justify-center items-center">
-      <button onClick={postData}>Post Email</button>
-    </div>
+    <div className="flex min-h-screen w-full justify-center items-center"></div>
   );
 }
